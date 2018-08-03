6b45108eb799d3b167b89884e32a136589c57f5c
asorianostratio <asoriano@stratio.com>
2018-08-03 23:09:22 +0200

    train

--- src/main/scala/com/stratio/intelligence/poc/SparkMlTrainingStrategies.scala
+++ src/main/scala/com/stratio/intelligence/poc/SparkMlTrainingStrategies.scala
@@ -26,53 +26,93 @@ case class EvalEvaluator(metricType: String,
 // ------------------------------------------------------
 // strategy = onlyTrain
 // strategy = trainAndEval
-// strategy = tuningHyperParamsTraining
+// strategy = tuningHyperParams
 //                validationMode = trainValidationSplit
 //                validationMode = crossValidation
 
-case class TrainingStrategy(strategy: String)
+trait TrainingStrategy
+
+case class OnlyTrain(strategy: String) extends TrainingStrategy{
+  assert("onlyTrain"==strategy)
+}
 
 case class TrainAndEvalStrategy(strategy: String,
-                           seed: Long,
-                           trainRatio: Double,
-                           evaluators: Array[EvalEvaluator]
-                          )
+                                seed: Long,
+                                trainRatio: Double,
+                                evaluators: Array[EvalEvaluator]
+                               ) extends TrainingStrategy{
+  assert("trainAndEval"==strategy)
+}
 
 case class TuningTrainValidationSplit(strategy: String,
-                                 validationMode: String,
-                                 seed: Long,
-                                 paramGrid: Map[String, String],
-                                 evaluator: EvalEvaluator,
-                                 trainRatio: Double
-                                )
+                                      validationMode: String,
+                                      seed: Long,
+                                      paramGrid: Map[String, String],
+                                      testEvaluator: EvalEvaluator,
+                                      trainRatio: Double,
+                                      evalRatio: Option[Double],
+                                      evaluators: Option[Array[EvalEvaluator]]
+                                     ) extends TrainingStrategy{
+  assert("tuningHyperParams"==strategy)
+  assert("trainValidationSplit"==validationMode)
+  if(evalRatio.isDefined) assert( evaluators.isDefined && evaluators.get.length > 1)
+
+}
 
 case class TuningCrossValidation(strategy: String,
                                  validationMode: String,
                                  seed: Long,
                                  paramGrid: Map[String, String],
-                                 evaluator: EvalEvaluator,
-                                 numFolds: Int
-                                )
+                                 testEvaluator: EvalEvaluator,
+                                 numFolds: Int,
+                                 evalRatio: Option[Double],
+                                 evaluators: Option[Array[EvalEvaluator]]
+                                ) extends TrainingStrategy{
+  assert("tuningHyperParams"==strategy)
+  assert("crossValidation"==validationMode)
+  if(evalRatio.isDefined) assert( evaluators.isDefined && evaluators.get.length > 1)
+}
 
 
 object SparkMlTrainingStrategies extends App {
 
   implicit val formats = DefaultFormats
 
-  val readedJson = Source.fromFile("examples/example_training_strategy.json").getLines.mkString
+  val onlyTrainingJson = Source.fromFile("examples/trainStrategy/only_train_strategy.json").getLines.mkString
+  val onlyTrainingStrategy = getTrainingStrategy(onlyTrainingJson)
+  assert(onlyTrainingStrategy.isSuccess)
 
-  val myObj = parse(readedJson).asInstanceOf[JObject]
-  val d = (myObj \ "strategy").extract[String] match {
-    case "onlyTrain" => Try(read[TrainingStrategy](readedJson))
-    case "trainAndEval" => Try(read[TrainAndEvalStrategy](readedJson))
-    case "tuningHyperParamsTraining" => {
-      (myObj \ "validationMode").extract[String] match {
-        case "trainValidationSplit" => Try(read[TuningTrainValidationSplit](readedJson))
-        case "crossValidation" => Try(read[TuningCrossValidation](readedJson))
+  val trainEvalJson = Source.fromFile("examples/trainStrategy/trainEvalSplit_train_strategy.json").getLines.mkString
+  val trainEvalStrategy = getTrainingStrategy(trainEvalJson)
+  assert(trainEvalStrategy.isSuccess)
+
+  val tuningTrainEvalJson = Source.fromFile("examples/trainStrategy/trainEvalSplit_tuning_strategy.json").getLines.mkString
+  val tuningTrainEvalStrategy = getTrainingStrategy(tuningTrainEvalJson)
+  assert(tuningTrainEvalStrategy.isSuccess)
+
+  val tuningCrossValidationJson = Source.fromFile("examples/trainStrategy/crossvalidation_tuning_strategy.json").getLines.mkString
+  val tuningCrossValidationStrategy = getTrainingStrategy(tuningCrossValidationJson)
+  assert(tuningCrossValidationStrategy.isSuccess)
+
+  // Errors
+  val onlyTrainingBadJson = Source.fromFile("examples/trainStrategy/withErrors/only_train_bad_strategy.json").getLines.mkString
+  val onlyTrainingBadStrategy = getTrainingStrategy(onlyTrainingBadJson)
+  assert(onlyTrainingBadStrategy.isFailure)
+
+
+  def getTrainingStrategy(trainStrategyJson: String): Try[TrainingStrategy] = Try{
+    val parsedJson = parse(trainStrategyJson).asInstanceOf[JObject]
+    (parsedJson \ "strategy").extract[String] match {
+      case "onlyTrain" => read[OnlyTrain](trainStrategyJson)
+      case "trainAndEval" => read[TrainAndEvalStrategy](trainStrategyJson)
+      case "tuningHyperParams" => {
+        (parsedJson \ "validationMode").extract[String] match {
+          case "trainValidationSplit" => read[TuningTrainValidationSplit](trainStrategyJson)
+          case "crossValidation" => read[TuningCrossValidation](trainStrategyJson)
+        }
       }
     }
   }
-
 
   print("a")
 }
